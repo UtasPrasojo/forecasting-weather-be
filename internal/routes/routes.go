55d0e02b715117d561611fe67ffc9b5717601cbe
UtasPrasojo <utas.prasojo@varx.id>
2026-02-25 15:57:20 +0700

    feat:deletes comment at routes.go

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 	_ "project-telkom-sigma/docs"
 	"project-telkom-sigma/internal/database"
-	"project-telkom-sigma/internal/handler" // Sesuaikan dengan path handler kamu
+	"project-telkom-sigma/internal/handler" 
 	"project-telkom-sigma/internal/repositories"
 	"project-telkom-sigma/internal/services"
 
@@ -14,19 +14,13 @@ import (
 func SetupRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.Handle("/swagger/", httpSwagger.WrapHandler)
-	// --- PROSES WIRING (Dependency Injection) ---
 
-	// 1. Inisialisasi Repository
 	weatherRepo := repositories.NewWeatherRepository(database.DB)
 
-	// 2. Inisialisasi Service (Masukkan Repo ke Service)
 	weatherService := services.NewWeatherService(weatherRepo)
 
-	// 3. Inisialisasi Handler (Masukkan Service ke Handler)
 	weatherHandler := handler.NewWeatherHandler(weatherService)
 
-	// --- PENDAFTARAN ROUTE ---
-	// Sekarang gunakan objek 'weatherHandler', bukan package 'handler'
 	mux.HandleFunc("POST /api/weather/sync", weatherHandler.HandleSync)
 	mux.HandleFunc("GET /api/weather", weatherHandler.GetAllWeather)
 	mux.HandleFunc("GET /api/weather/detail", weatherHandler.GetWeatherByID)
